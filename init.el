45f32948413223aefb0f52462b0e3776b77b9ccd
Nasos Thomos <nasos.thomos153@gmail.com>
2018-10-29 15:14:08 -0400

    Basic configuration with neotree package and zenburn theme.

--- init.el
+++ init.el
@@ -4,8 +4,17 @@
 (package-initialize)
 ;; Set F8 to toggle the nice feature of neotree.
 (global-set-key [f8] 'neotree-toggle)
-;; Load the monokai theme
-(load-theme 'monokai t)
 
-(require 'rtags)
-(cmake-ide-setup)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (auto-complete neotree zenburn-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(load-theme 'zenburn t)
